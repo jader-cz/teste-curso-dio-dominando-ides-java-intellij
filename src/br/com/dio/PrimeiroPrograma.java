3e1fb6c80924becb3291fc8d9b88c72f74f1f771
jader <jader-cz@hotmail.com>
2023-02-28 16:39:25 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+        /*int a = 3;
+        int b = 5;
+
+        System.out.println("Helo word! " + (a+b));*/
+    }
+}
